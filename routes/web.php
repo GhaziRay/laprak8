101142adf40821f6c6c849ff6961c7576e85d73d
Ghazi <ghazirayshafiq@gmail.com>
2026-01-05 17:29:08 +0700

    FIX

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController; // <--- PENTING: Tambahkan ini
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('products', ProductController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
